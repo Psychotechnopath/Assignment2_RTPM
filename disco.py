ada1dfaf931542a410f0c841c6dc816a4ce39a99
Psychotechnopath <yme.linden@protonmail.com>
2020-11-17 22:22:10 +0100

    Pre-processed data

--- disco.py
+++ disco.py
@@ -1,8 +1,88 @@
+#%%
 import pandas as pd
-import pm4py
 
-from pm4py.objects.log.importer.xes import factory as xes_import_factory
-log = xes_import_factory.apply("BPI Challenge 2018.xes")
+UD1_df = pd.read_csv('Filtered dataset UD I.csv')
+UD1_df_case_id = list(UD1_df['Case ID'])
 
-print("test")
-#Test 1
+UD2_df = pd.read_csv('UD2 only (filtered).csv')
+UD2_df_case_id = list(UD2_df['Case ID'])
+
+total_df = pd.read_csv('All cases.csv') #Set CaseId to index col
+total_df_index = total_df.set_index('Case ID', drop=False)
+
+#%%
+#Only keep last activity
+total_df_sub = total_df_index.groupby(total_df_index.index).last()
+#Set timestamps as index
+total_df_sub.set_index('Complete Timestamp', inplace=True)
+#Remove 1 duplicate time value
+total_df_sub = total_df_sub.groupby(total_df_sub.index).last()
+
+#Drop unwanted columns
+total_df_sub.drop(columns=['Variant', '(case) amount_applied1',
+                           '(case) amount_applied2',
+                           '(case) amount_applied3',
+                           '(case) applicant',
+                           '(case) application',
+                           '(case) payment_actual1',
+                           '(case) payment_actual2',
+                           '(case) payment_actual3',
+                           '(case) penalty_amount1',
+                           '(case) penalty_amount2',
+                            '(case) penalty_amount3',
+                           '(case) program-id',
+                           '(case) year',
+                           'docid',
+                           'doctype',
+                           'lifecycle:transition',
+                           'note'], inplace=True)
+
+# If Case ID in UD1_df_case_id -> UD1
+# If Case ID in UD2_df_case_id -> UD2
+# Else Normal
+
+# 0 = Normal
+# 1 = UD1
+# 2 = UD2
+payment_class = []
+for index, value in enumerate(total_df_sub['Case ID']):
+    if value in UD1_df_case_id:
+        payment_class.append(1)
+    elif value in UD2_df_case_id:
+        payment_class.append(2)
+    else:
+        payment_class.append(0)
+
+total_df_sub['payment_class'] = payment_class
+
+total_df_sub[['(case) rejected', 'success', '(case) basic payment',
+       '(case) greening', '(case) penalty_ABP', '(case) penalty_AGP', '(case) penalty_AJLP',
+       '(case) penalty_AUVP', '(case) penalty_AVBP', '(case) penalty_AVGP',
+       '(case) penalty_AVJLP', '(case) penalty_AVUVP', '(case) penalty_B16',
+       '(case) penalty_B2', '(case) penalty_B3', '(case) penalty_B4',
+       '(case) penalty_B5', '(case) penalty_B5F', '(case) penalty_B6',
+       '(case) penalty_BGK', '(case) penalty_BGKV', '(case) penalty_BGP',
+       '(case) penalty_C16', '(case) penalty_C4', '(case) penalty_C9',
+       '(case) penalty_CC', '(case) penalty_GP1', '(case) penalty_JLP1',
+       '(case) penalty_JLP2', '(case) penalty_JLP3', '(case) penalty_JLP5',
+       '(case) penalty_JLP6', '(case) penalty_JLP7', '(case) penalty_V5',
+        '(case) redistribution', '(case) selected_manually',
+       '(case) selected_random', '(case) selected_risk', '(case) small farmer',
+       '(case) young farmer']] = \
+    total_df_sub[['(case) rejected', 'success',
+       '(case) basic payment',
+       '(case) greening', '(case) penalty_ABP', '(case) penalty_AGP', '(case) penalty_AJLP',
+       '(case) penalty_AUVP', '(case) penalty_AVBP', '(case) penalty_AVGP',
+       '(case) penalty_AVJLP', '(case) penalty_AVUVP', '(case) penalty_B16',
+       '(case) penalty_B2', '(case) penalty_B3', '(case) penalty_B4',
+       '(case) penalty_B5', '(case) penalty_B5F', '(case) penalty_B6',
+       '(case) penalty_BGK', '(case) penalty_BGKV', '(case) penalty_BGP',
+       '(case) penalty_C16', '(case) penalty_C4', '(case) penalty_C9',
+       '(case) penalty_CC', '(case) penalty_GP1', '(case) penalty_JLP1',
+       '(case) penalty_JLP2', '(case) penalty_JLP3', '(case) penalty_JLP5',
+       '(case) penalty_JLP6', '(case) penalty_JLP7', '(case) penalty_V5',
+         '(case) redistribution', '(case) selected_manually',
+       '(case) selected_random', '(case) selected_risk', '(case) small farmer',
+       '(case) young farmer']].astype(int)
+
+total_df_sub.to_csv('Checkpoint.csv', header=True)
---
+++ scratchpad.py
@@ -0,0 +1,111 @@
+for index, row in total_df_sub.iterrows():
+    if row['(case) greening']:
+        application_types.append(1)
+    if row['(case) redistribution']:
+        application_types.append(2)
+    if row['(case) small farmer']:
+        application_types.append(3)
+    if row['(case) young farmer']:
+        application_types.append(4)
+    if row['(case) basic payment']:
+        application_types.append(5)
+    if row['(case) selected_random']:
+        selection_types.append(1)
+    if row['(case) selected_risk']:
+        selection_types.append(2)
+    if row['(case) selected_manually']:
+        selection_types.append(3)
+    if row['(case) penalty_ABP']:
+        penalty_types.append(1)
+        continue
+    if row['(case) penalty_V5']:
+        penalty_types.append(2)
+        continue
+    if row['(case) penalty_AGP']:
+        penalty_types.append(3)
+        continue
+    if row['(case) penalty_AJLP']:
+        penalty_types.append(4)
+        continue
+    if row['(case) penalty_AUVP']:
+        penalty_types.append(5)
+        continue
+    if row['(case) penalty_AVBP']:
+        penalty_types.append(6)
+        continue
+    if row['(case) penalty_AVGP']:
+        penalty_types.append(7)
+        continue
+    if row['(case) penalty_AVJLP']:
+        penalty_types.append(8)
+        continue
+    if row['(case) penalty_AVUVP']:
+        penalty_types.append(9)
+        continue
+    if row['(case) penalty_B16']:
+        penalty_types.append(10)
+        continue
+    if row['(case) penalty_B2']:
+        penalty_types.append(11)
+        continue
+    if row['(case) penalty_B3']:
+        penalty_types.append(12)
+        continue
+    if row['(case) penalty_B4']:
+        penalty_types.append(13)
+        continue
+    if row['(case) penalty_B5']:
+        penalty_types.append(14)
+        continue
+    if row['(case) penalty_B5F']:
+        penalty_types.append(15)
+        continue
+    if row['(case) penalty_B6']:
+        penalty_types.append(16)
+        continue
+    if row['(case) penalty_BGK']:
+        penalty_types.append(17)
+        continue
+    if row['(case) penalty_BGKV']:
+        penalty_types.append(18)
+        continue
+    if row['(case) penalty_BGP']:
+        penalty_types.append(19)
+        continue
+    if row['(case) penalty_C16']:
+        penalty_types.append(20)
+        continue
+    if row['(case) penalty_C4']:
+        penalty_types.append(21)
+        continue
+    if row['(case) penalty_C9']:
+        penalty_types.append(22)
+        continue
+    if row['(case) penalty_CC']:
+        penalty_types.append(23)
+        continue
+    if row['(case) penalty_GP1']:
+        penalty_types.append(24)
+        continue
+    if row['(case) penalty_JLP1']:
+        penalty_types.append(25)
+        continue
+    if row['(case) penalty_JLP2']:
+        penalty_types.append(26)
+        continue
+    if row['(case) penalty_JLP3']:
+        penalty_types.append(27)
+        continue
+    if row['(case) penalty_JLP5']:
+        penalty_types.append(28)
+        continue
+    if row['(case) penalty_JLP6']:
+        penalty_types.append(29)
+        continue
+    if row['(case) penalty_JLP7']:
+        penalty_types.append(30)
+        continue
+    else:
+        penalty_types.append(0)
+        selection_types.append(0)
+        application_types.append(0)
